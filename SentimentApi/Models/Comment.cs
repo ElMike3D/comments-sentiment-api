5da9ca71c9967f8fb9144d5b1bc5e676fa1497a6
Miguel Muñoz <nanopizzafuturo@gmail.com>
2025-09-23 20:09:00 -0600

    feat: agrega modelo Comment, AppDbContext y configuración inicial de SQL Server

---
+++ SentimentApi/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace SentimentApi.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string ProductId { get; set; }
+        public string UserId { get; set; }
+        public string CommentText { get; set; }
+        public string Sentiment { get; set; }
+        public float? Rating { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- SentimentApi/Program.cs
+++ SentimentApi/Program.cs
@@ -1,11 +1,16 @@
+using SentimentApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
@@ -17,9 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
